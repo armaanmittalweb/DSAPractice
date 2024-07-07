4c57f558e28e2650388f6cb05744bc3f668a3191
armaanmittalweb <121077650+armaanmittalweb@users.noreply.github.com>
2024-07-07 16:26:36 +0530

    Create 07-07-24_01.cpp

---
+++ LeetCode/07-07-24_01.cpp
@@ -0,0 +1,16 @@
+// Problem Link: https://leetcode.com/problems/water-bottles
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int left = numBottles;
+        int ans = numBottles;
+        while(left>=numExchange){
+            ans += left/numExchange;
+            left = (left/numExchange)  + left%numExchange;
+        }
+
+        return ans;
+
+    }
+};
