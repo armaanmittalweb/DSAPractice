241d15ae9837145de1c7e6a1450e164cdbf2fc5a
armaanmittalweb <121077650+armaanmittalweb@users.noreply.github.com>
2024-07-25 12:33:00 +0530

    Create 25-07-24_01.cpp

---
+++ LeetCode/25-07-24_01.cpp
@@ -0,0 +1,9 @@
+// Problme Link: https://leetcode.com/problems/sort-an-array
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+       return nums;
+    }
+};
