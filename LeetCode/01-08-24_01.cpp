bc5f5339e8089303cddfc3abd2b6f4aaebf22701
armaanmittalweb <121077650+armaanmittalweb@users.noreply.github.com>
2024-08-01 21:58:52 +0530

    Create 01-08-24_01.cpp

---
+++ LeetCode/01-08-24_01.cpp
@@ -0,0 +1,12 @@
+// Problem Link: https://leetcode.com/problems/number-of-senior-citizens
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans = 0;
+        for (auto& x : details) {
+            int age = stoi(x.substr(11, 2));
+            ans += age > 60;
+        }
+        return ans;
+    }
+};
