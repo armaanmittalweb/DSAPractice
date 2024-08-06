c4c0b9b5a321b0d03fc13a5c00d9782105e6176e
armaanmittalweb <121077650+armaanmittalweb@users.noreply.github.com>
2024-08-06 23:52:50 +0530

    Create 06-08-24_01.cpp

---
+++ LeetCode/06-08-24_01.cpp
@@ -0,0 +1,15 @@
+// Problem Link: https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii
+vector<int> letterFreq(26, 0);
+        for (char c : word) {
+            letterFreq[c - 'a']++;
+        }
+        
+        sort(letterFreq.rbegin(), letterFreq.rend());
+        
+        int totalPresses = 0;
+        for (int i = 0; i < 26; i++) {
+            if (letterFreq[i] == 0) break;
+            totalPresses += (i / 8 + 1) * letterFreq[i];
+        }
+        
+        return totalPresses;
